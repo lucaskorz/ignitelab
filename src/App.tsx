068e4b22876e5f7e2eb0a1901f40af23290d896b
Lucas Korz <62682384+lucaskorz@users.noreply.github.com>
2022-06-24 20:44:15 -0300

    finalizando aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { ApolloProvider } from "@apollo/client";
 import { BrowserRouter } from "react-router-dom";
-import { Router } from "./components/Router";
+import { Router } from "./Router";
 import { client } from "./lib/apollo";
 
 function App() {
--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,4 +1,5 @@
 import { DefaultUi, Player, Youtube } from "@vime/react";
+<<<<<<< HEAD
 import { CaretRight, DiscordLogo, FileArrowDown, Lightning } from "phosphor-react";
 
 import '@vime/core/themes/default.css'
@@ -31,12 +32,24 @@ interface GetLessonBySlugResponse {
     }
   }
 }
+=======
+import {
+  CaretRight,
+  DiscordLogo,
+  FileArrowDown,
+  Lightning,
+} from "phosphor-react";
+
+import "@vime/core/themes/default.css";
+import { useGetLessonBySlugQuery } from "../graphql/generated";
+>>>>>>> b9c0765 (finalizando aplicação)
 
 interface VideoProps {
   lessonSlug: string;
 }
 
 export function Video(props: VideoProps) {
+<<<<<<< HEAD
   const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
     variables: {
       slug: props.lessonSlug
@@ -44,11 +57,24 @@ export function Video(props: VideoProps) {
   })
 
   if (!data) {
+=======
+  const { data } = useGetLessonBySlugQuery({
+    variables: {
+      slug: props.lessonSlug,
+    },
+  });
+
+  if (!data || !data.lesson) {
+>>>>>>> b9c0765 (finalizando aplicação)
     return (
       <div className="flex-1">
         <p>Carregando...</p>
       </div>
+<<<<<<< HEAD
     )
+=======
+    );
+>>>>>>> b9c0765 (finalizando aplicação)
   }
 
   return (
@@ -65,13 +91,18 @@ export function Video(props: VideoProps) {
       <div className="p-8 max-w-[1100px] mx-auto">
         <div className="flex items-start gap-16">
           <div className="flex-1">
+<<<<<<< HEAD
             <h1 className="text-2xl font-bold">
               {data?.lesson?.title}
             </h1>
+=======
+            <h1 className="text-2xl font-bold">{data?.lesson?.title}</h1>
+>>>>>>> b9c0765 (finalizando aplicação)
             <p className="mt-4 text-gray-200 leading-relaxed">
               {data?.lesson?.description}
             </p>
 
+<<<<<<< HEAD
             <div className="flex items-center gap-4 mt-6">
               <img 
                 className="h-16 w-16 rounded-full border-2 border-blue-500"
@@ -82,57 +113,83 @@ export function Video(props: VideoProps) {
               <div className="leading-relaxed">
                 <strong className="font-bold text-2xl block">{data?.lesson?.teacher?.name}</strong>
                 <span className="text-gray-200 text-sm block">{data?.lesson?.teacher?.bio}</span>
+=======
+            {data.lesson.teacher && (
+              <div className="flex items-center gap-4 mt-6">
+                <img
+                  className="h-16 w-16 rounded-full border-2 border-blue-500"
+                  src={data?.lesson?.teacher?.avatarURL}
+                  alt=""
+                />
+
+                <div className="leading-relaxed">
+                  <strong className="font-bold text-2xl block">
+                    {data?.lesson?.teacher?.name}
+                  </strong>
+                  <span className="text-gray-200 text-sm block">
+                    {data?.lesson?.teacher?.bio}
+                  </span>
+                </div>
+>>>>>>> b9c0765 (finalizando aplicação)
               </div>
-            </div>
+            )}
           </div>
 
           <div className="flex flex-col gap-4">
             <a
               href=""
-              className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors">
+              className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors"
+            >
               <DiscordLogo size={24} />
               Comunidade do Discord
             </a>
             <a
               href=""
-              className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-500 hover:text-gray-900 transition-colors">
+              className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-500 hover:text-gray-900 transition-colors"
+            >
               <Lightning size={24} />
               Acesse o desafio
             </a>
           </div>
         </div>
         <div className="gap-8 mt-20 grid grid-cols-2">
-          <a href="" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
+          <a
+            href=""
+            className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors"
+          >
             <div className="bg-green-700 h-full p-6 flex items-center">
               <FileArrowDown size={40} />
             </div>
             <div className="py-6 leading-relaxed">
               <strong className="text-2xl">Material complementar</strong>
               <p className="text-sm text-gray-200 mt-2">
-                Acesse o material complementar para acelerar o seu desenvolvimento
+                Acesse o material complementar para acelerar o seu
+                desenvolvimento
               </p>
             </div>
             <div className="h-full p-6 flex items-center">
               <CaretRight size={24} />
             </div>
           </a>
-          <a href="" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
+          <a
+            href=""
+            className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors"
+          >
             <div className="bg-green-700 h-full p-6 flex items-center">
               <FileArrowDown size={40} />
             </div>
             <div className="py-6 leading-relaxed">
               <strong className="text-2xl">Wallpapers exclusivos</strong>
               <p className="text-sm text-gray-200 mt-2">
-                Baixe wallpapers exclusivos do Ignite Lab e personalize a sua máquina
+                Baixe wallpapers exclusivos do Ignite Lab e personalize a sua
+                máquina
               </p>
             </div>
             <div className="h-full p-6 flex items-center">
               <CaretRight size={24} />
             </div>
           </a>
-          <a href="">
-
-          </a>
+          <a href=""></a>
         </div>
       </div>
     </div>
